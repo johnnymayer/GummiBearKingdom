d207fd460cf0edc74ecd413e0b7bed26ad51d55b
Johnny Mayer <johnnymayer@gmail.com>
2018-04-20 11:37:33 -0700

    Scaffold fix.

---
+++ GummiBearKingdom/Controllers/GummisController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Controllers
+{
+    public class GummisController : Controller
+    {
+        private readonly GummiBearKingdomDbContext _context;
+
+        public GummisController(GummiBearKingdomDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Gummis
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Gummis.ToListAsync());
+        }
+
+        // GET: Gummis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gummi = await _context.Gummis
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (gummi == null)
+            {
+                return NotFound();
+            }
+
+            return View(gummi);
+        }
+
+        // GET: Gummis/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gummis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Description")] Gummi gummi)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gummi);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(gummi);
+        }
+
+        // GET: Gummis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gummi = await _context.Gummis.SingleOrDefaultAsync(m => m.Id == id);
+            if (gummi == null)
+            {
+                return NotFound();
+            }
+            return View(gummi);
+        }
+
+        // POST: Gummis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Description")] Gummi gummi)
+        {
+            if (id != gummi.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gummi);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GummiExists(gummi.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(gummi);
+        }
+
+        // GET: Gummis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gummi = await _context.Gummis
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (gummi == null)
+            {
+                return NotFound();
+            }
+
+            return View(gummi);
+        }
+
+        // POST: Gummis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gummi = await _context.Gummis.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Gummis.Remove(gummi);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool GummiExists(int id)
+        {
+            return _context.Gummis.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GummiBearKingdom/Migrations/20180420183159_Secondary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GummiBearKingdom.Migrations
+{
+    public partial class Secondary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Gummis",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGeneratedOnAdd", true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Gummis",
+                nullable: true,
+                oldClrType: typeof(string))
+                .OldAnnotation("MySql:ValueGeneratedOnAdd", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Gummis");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Gummis",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true)
+                .Annotation("MySql:ValueGeneratedOnAdd", true);
+        }
+    }
+}
