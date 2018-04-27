02bf2a05d09b88f312e5b182ee0af04e6b3034b5
Johnny Mayer <johnnymayer@gmail.com>
2018-04-27 12:02:34 -0700

    First passing test for Gummi constructor

---
+++ GummiBearKingdom.Tests/ModelTests/GummiTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Tests
+{
+    [TestClass]
+    public class GummiTests
+    {
+        
+        [TestMethod]
+        public void GetProperties_ReturnsData_All()
+        {
+            //Arrange
+            Gummi newGummi = new Gummi("Redd", "A strong red Gummi", 1);
+
+            //Act
+            int idResult = newGummi.GummiId;
+            string nameResult = newGummi.Name;
+            int priceResult = newGummi.Price;
+            string descriptionResult = newGummi.Description;
+
+            //Assert
+            Assert.AreEqual(0, idResult);
+            Assert.AreEqual("Redd", nameResult);
+            Assert.AreEqual(1, priceResult);
+            Assert.AreEqual("A strong red Gummi", descriptionResult);
+
+        }
+    }
+}
--- GummiBearKingdom/Models/Gummi.cs
+++ GummiBearKingdom/Models/Gummi.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,18 +10,38 @@ namespace GummiBearKingdom.Models
     [Table("Gummis")]
     public class Gummi
     {
-
-        public Gummi()
-        {
-            this.Reviews = new HashSet<Review>();
-        }
-
-
+        
         [Key]
         public int GummiId { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Review> Reviews { get; set; }
-    }
+
+        public Gummi(string name, string description, int price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            GummiId = 0;
+        }
+
+		public override bool Equals(object otherGummi)
+		{
+            if (!(otherGummi is Gummi))
+            {
+                return false;
+            }
+            else
+            {
+                Gummi newGummi = (Gummi)otherGummi;
+                return this.GummiId.Equals(newGummi.GummiId);
+            }
+		}
+
+		public override int GetHashCode()
+		{
+            return this.GummiId.GetHashCode();
+		}
+	}
 }
