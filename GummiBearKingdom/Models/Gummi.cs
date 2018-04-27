bb1440d5019deac189206e02e9650e54a8b4d24d
Johnny Mayer <johnnymayer@gmail.com>
2018-04-27 09:10:30 -0700

    Begin DB restructure to add Review model

--- GummiBearKingdom/Models/Gummi.cs
+++ GummiBearKingdom/Models/Gummi.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GummiBearKingdom.Models
 {
+    [Table("Gummis")]
     public class Gummi
     {
         [Key]
@@ -11,6 +14,6 @@ namespace GummiBearKingdom.Models
         public string Name { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
-
+        public virtual Review Review { get; set; }
     }
 }
---
+++ GummiBearKingdom/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {
+        public Review()
+        {
+            this.Gummis = new HashSet<Gummi>();
+        }
+
+        public int AuthorId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public virtual ICollection<Gummi> Gummis { get; set; }
+
+    }
+}
