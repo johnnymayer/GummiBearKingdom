ef189b5175eb941a0261367067fdddb01dd68532
Johnny Mayer <johnnymayer@gmail.com>
2018-04-27 12:39:20 -0700

    All tests passing for Gummi Model

--- GummiBearKingdom.Tests/ModelTests/GummiTests.cs
+++ GummiBearKingdom.Tests/ModelTests/GummiTests.cs
@@ -9,7 +9,7 @@ namespace GummiBearKingdom.Tests
     {
         
         [TestMethod]
-        public void GetProperties_ReturnsData_All()
+        public void GummiGetter_ReturnsData_All()
         {
             //Arrange
             Gummi newGummi = new Gummi("Redd", "A strong red Gummi", 1);
@@ -25,7 +25,35 @@ namespace GummiBearKingdom.Tests
             Assert.AreEqual("Redd", nameResult);
             Assert.AreEqual(1, priceResult);
             Assert.AreEqual("A strong red Gummi", descriptionResult);
-
         }
+
+        [TestMethod]
+        public void GummiSetter_SetsData_All()
+        {
+            //Arrange
+            Gummi newGummi = new Gummi("Redd", "A strong red Gummi", 1);
+
+            //Act
+            newGummi.Name = "Red Name";
+            newGummi.Description = "Red Description";
+            newGummi.Price = 2;
+
+            //Assert
+            Assert.AreEqual("Red Name", newGummi.Name);
+            Assert.AreEqual("Red Description", newGummi.Description);
+            Assert.AreEqual(2, newGummi.Price);
+        }
+
+        [TestMethod]
+        public void GummiConstructor_ReturnsGummi_newGummi()
+        {
+            //Arrange
+            Gummi newGummi = new Gummi("Redd", "A strong red Gummi", 1);
+
+            //Assert
+            Assert.AreEqual("Redd", newGummi.Name);
+        }
+
+
     }
 }
